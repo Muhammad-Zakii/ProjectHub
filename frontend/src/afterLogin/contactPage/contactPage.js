678dac957c5b9948cf9a38088b8a6583f8a0656e
zakiimalik <zakimalik853@gmail.com>
2022-09-02 02:02:17 +0500

    updated

--- frontend/src/afterLogin/contactPage/contactPage.js
+++ frontend/src/afterLogin/contactPage/contactPage.js
@@ -1,10 +1,62 @@
 import React from 'react'
 
+import NavBarr from '../navbars/navbar'
+import { Card, Row, Col, Button } from 'react-bootstrap'
+import FormRow from '../../formrow'
+import '../../index.css'
+import Footer from '../footer/footer2'
+
 const ContactPage = () => {
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <>
+      <NavBarr />
+      <Row className='m-5'>
+        <Col>
+          <h4>Profile</h4>
+          <div className='clearfix'>
+            <div className='row'>
+              <div className='col-md-4 animated fadeIn'>
+                <div className='card'>
+                  <div className='card-body'>
+                    <div className='avatar'>
+                      <img src='' className='card-img-top' alt='' />
+                    </div>
+                    <h5 className='card-title'>Muhammad Zaki</h5>
+                    <p className='card-text'>
+                      Gujranwala
+                      <br />
+                      <p className='phone'>03167141511</p>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col>
+          <FormRow type='fname' name='fname' placeholder='First name' />
+          <FormRow type='lname' name='lname' placeholder='Last name' />
+          <FormRow
+            type='email'
+            name='email'
+            placeholder='Email of the Seller'
+          />
+          <FormRow type='subject' name='subject' placeholder='Subject' />
+          <div className='form-row'>
+            <label className='form-label'>Message</label>
+
+            <textarea
+              className='form-textarea'
+              type='text'
+              name='summary'
+              placeholder='Message'
+            />
+          </div>
+          <Button variant='primary'>Send Message</Button>
+        </Col>
+      </Row>
+      <Footer />
+    </>
   )
 }
 
